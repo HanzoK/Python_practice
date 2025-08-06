5650d912311a6d16665a679c84befbef30adc253
Hanju Kim <hanjkim@c1r2p6.42vienna.com>
2025-08-06 18:09:02 +0200

    pushing a finished hangman project

--- 05_hangman/00_hangman.py
+++ 05_hangman/00_hangman.py
@@ -1,20 +1,50 @@
 #! usr/bin/env python 3
 
 import random
+import hangman_art
+import hangman_words
 
-word_list = ["aardvark", "baboon", "camel"]
-
+word_list = hangman_words.word_list
 chosen_word = random.choice(word_list)
-print(chosen_word)
+stages = hangman_art.stages
+lives = 6
+game_over = False
+correct_letters = []
 
-input = input("Guess a letter:\n").lower()
+while not game_over:
+    print(f"******************** {lives} / 6 LIVES LEFT*********************\n")
+    guess = input("Guess a letter: ").lower()
 
-if len(input) != 1:
-    print("1 letter, do I have to repeat myself?")
-    exit()
+    if len(guess) != 1:
+        print(stages[lives])
+        print("1 letter, do I have to repeat myself?")
+        continue
+    elif guess in correct_letters:
+        print(stages[lives])
+        print("You already guessed that letter.")
+        continue
 
-for i in range(0, len(chosen_word)):
-    if input == chosen_word[i]:
-        print("Right.")
-    else:
-        print("Wrong.")
+    display = ""
+
+    for letter in chosen_word:
+        if letter == guess:
+            display += letter
+            correct_letters.append(guess)
+        elif letter in correct_letters:
+            display += letter
+        else:
+            display += "_"
+
+    print(display + "\n")
+
+    if guess not in chosen_word:
+        lives -= 1
+        if lives == 0:
+            print(f"HAH! Loser. The word was '{chosen_word}'.")
+            game_over = True
+
+    if "_" not in display:
+        game_over = True
+        print("Congrats. You win!")
+
+    print(stages[lives])
