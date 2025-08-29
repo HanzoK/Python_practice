4a3203048eb4eed9d733d738a91db52fc2d1d044
HanzoK <hanju.soonsik.kim@gmail.com>
2025-08-29 23:53:21 +0200

    finished coffee machine with objects. Way faster

--- intermediate/01_coffee_machine_OOP/main.py
+++ intermediate/01_coffee_machine_OOP/main.py
@@ -1,4 +1,24 @@
 from menu import Menu
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+from art import logo
 
+menu = Menu()
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+
+is_on = True
+
+while is_on:
+    choice = input(f"What would you like? {menu.get_items()}: ").lower()
+    if choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+    elif choice == "off":
+        is_on = False
+    elif choice == "espresso" or choice == "latte" or choice == "cappuccino":
+        drink = menu.find_drink(choice)
+        if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+            coffee_maker.make_coffee(drink)
+    else:
+        print("Beep Boop. Unknown command. Please try again.")
