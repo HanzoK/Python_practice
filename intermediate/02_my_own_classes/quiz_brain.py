0dd26fe8df40c8a13e807e680d881d8db51bad53
HanzoK <hanju.soonsik.kim@gmail.com>
2025-08-31 20:31:37 +0200

    Started a quiz game. implemented classes with methods and attributes

---
+++ intermediate/02_my_own_classes/quiz_brain.py
@@ -0,0 +1,14 @@
+class QuizBrain:
+    def __init__(self, quiz_list):
+        self.question_number = 0
+        self.question_list = quiz_list
+
+    def still_has_questions(self):
+        if self.question_number == self.question_list[-1]:
+            return False
+        return True
+
+    def next_question(self):
+        current_question = self.question_list[self.question_number]
+        self.question_number += 1
+        input(f"Q.{self.question_number}: {current_question.text} (True/False)?: ")
