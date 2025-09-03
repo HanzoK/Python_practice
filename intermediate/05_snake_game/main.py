43e269469eccbca156f0791bbfb5dba0e9322d30
HanzoK <hanju.soonsik.kim@gmail.com>
2025-09-03 17:12:06 +0200

    .

--- intermediate/05_snake_game/main.py
+++ intermediate/05_snake_game/main.py
@@ -3,7 +3,7 @@ from turtle import Turtle, Screen
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
-screen.title("Snake Game")
+screen.title("HanzoK's Snake Game")
 
 
 
