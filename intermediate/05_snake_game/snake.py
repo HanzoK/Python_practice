871887e5bb0f0bafbdeb11d22ca49e34de9d8bb4
HanzoK <hanju.soonsik.kim@gmail.com>
2025-09-06 22:19:11 +0200

    added direction movement and collision detected with own body when changing directions

--- intermediate/05_snake_game/snake.py
+++ intermediate/05_snake_game/snake.py
@@ -1,8 +1,17 @@
 from turtle import Turtle
 
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
 class Snake:
     def __init__(self):
         self.snake = []
+        self.create_snake()
+        self.head = self.snake[0]
+
+    def create_snake(self):
         x = 0
         for _ in range(3):
             segment = Turtle(shape="square")
@@ -17,4 +26,20 @@ class Snake:
             new_x = self.snake[segment - 1].xcor()
             new_y = self.snake[segment - 1].ycor()
             self.snake[segment].goto(new_x, new_y)
-        self.snake[0].forward(20)
+        self.head.forward(20)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
