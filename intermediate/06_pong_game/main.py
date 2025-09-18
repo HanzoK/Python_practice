0ae044c642cb03d7f7481463d208fb5196887d1e
HanzoK <hanju.soonsik.kim@gmail.com>
2025-09-18 18:46:01 +0200

    finished pong game

--- intermediate/06_pong_game/main.py
+++ intermediate/06_pong_game/main.py
@@ -1,23 +1,38 @@
 from turtle import Screen
 from player import Player
+from ball import Ball
+from scoreboard import Scoreboard
+import time
 
 screen = Screen()
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
 screen.title("Pong")
+screen.tracer(0)
 
-
-paddle_1 = Player()
-paddle_2 = Player()
-paddle_1.goto(350, 0)
-paddle_2.goto(-350, 0)
-paddle_1.showturtle()
-paddle_2.showturtle()
+paddle_r = Player((350, 0))
+paddle_l = Player((-350, 0))
+ball = Ball()
+scorebaord = Scoreboard()
 
 screen.listen()
-screen.onkey(paddle_1.up, "Up")
-screen.onkey(paddle_1.down, "Down")
-screen.onkey(paddle_2.up, "w")
-screen.onkey(paddle_2.down, "s")
+screen.onkey(paddle_r.up, "Up")
+screen.onkey(paddle_r.down, "Down")
+screen.onkey(paddle_l.up, "w")
+screen.onkey(paddle_l.down, "s")
+
+game_is_on = True
+while game_is_on:
+    time.sleep(ball.move_speed)
+    screen.update()
+    ball.move()
+    if ball.distance(paddle_r) < 50 and ball.xcor() > 335 or ball.distance(paddle_l) < 50 and ball.xcor() < -335:
+        ball.bounce_x()
+    if ball.xcor() > 380:
+        ball.reset_pos()
+        scorebaord.l_point()
+    if ball.xcor() < -390:
+        ball.reset_pos()
+        scorebaord.r_point()
 
 screen.exitonclick()
--- intermediate/06_pong_game/player.py
+++ intermediate/06_pong_game/player.py
@@ -4,14 +4,14 @@ UP = 90
 DOWN = 270
 
 class Player(Turtle):
-    def __init__(self):
+    def __init__(self, position):
         super().__init__()
-        self.hideturtle()
         self.shape("square")
         self.color("white")
         self.shapesize(stretch_wid=5, stretch_len=1)
         self.penup()
-        self.speed("fastest")
+        self.speed("fastest")   
+        self.goto(position)
 
     def up(self):
         self.sety(self.ycor() + 20)
