939b7cfd313ed1d56ce7c3ba41078aff1fc1c70e
HanzoK <hanju.soonsik.kim@gmail.com>
2025-09-23 14:43:09 +0200

    implemented reading from high_score file to keep track of last high score achieved

--- intermediate/05_snake_game/scoreboard.py
+++ intermediate/05_snake_game/scoreboard.py
@@ -6,7 +6,8 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.current_score = 0
-        self.high_score = 0
+        with open("data.txt") as high_score_file:
+            self.high_score = int(high_score_file.read())
         self.color("white")
         self.hideturtle()
         self.penup()
@@ -20,6 +21,8 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.current_score > self.high_score:
             self.high_score = self.current_score
+        with open("data.txt", mode="w") as high_score_file:
+            high_score_file.write(str(self.high_score))
         self.current_score = 0
         self.update()
 
