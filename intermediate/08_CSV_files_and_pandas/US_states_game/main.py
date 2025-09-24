38271d04801baacdd0758bf2f4aadcebcf7109f0
HanzoK <hanju.soonsik.kim@gmail.com>
2025-09-25 01:00:16 +0200

    optimized logic for readability and speed. Added functionality to save states not guessed in a separate csv file to learn later

--- intermediate/08_CSV_files_and_pandas/US_states_game/main.py
+++ intermediate/08_CSV_files_and_pandas/US_states_game/main.py
@@ -14,24 +14,28 @@ guessed_states = []
 
 def write_name(guess):
     state_to_be_created = data[data["state"] == f"{guess}"]
-    state_x = state_to_be_created.x.iloc[0]
-    state_y = state_to_be_created.y.iloc[0]
-    new_state = state_class.state(answer_state, state_x, state_y)
+    state_x = state_to_be_created.x.item()
+    state_y = state_to_be_created.y.item() 
+    new_state = state_class.state(guess, state_x, state_y)
 
-score = 0
 game_is_on = True
-while game_is_on:
-    if score == 50:
-        game_is_on = False
-    answer_state = screen.textinput(title=f"Guess the state ({score}/50)", prompt="What's another state's name?").title()
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"({len(guessed_states)}/50) guessed States", 
+                                    prompt="What's another state's name?").title()
+    if answer_state == "Exit":
+        break
     if answer_state in guessed_states:
         print("You already guessed that state!")
     elif answer_state in data["state"].values:
         write_name(answer_state)
         guessed_states.append(answer_state)
-        score += 1
-        print(guessed_states)
     else:
         continue
-        
-screen.exitonclick()
+
+# states_to_learn.csv
+states_to_learn = []
+for item in data["state"]:
+    if item not in guessed_states:
+        states_to_learn.append(item)
+new_csv = pandas.DataFrame(states_to_learn)
+new_csv.to_csv("states_to_learn.csv")
