f8d9361fa02a15db01d91d769373a128713e12ff
HanzoK <hanju.soonsik.kim@gmail.com>
2025-09-26 22:29:51 +0200

    completed NATO alphabet project using dict comprehension

---
+++ intermediate/09_list_comprehension_and_NATO/NATO_alphabet_project/main.py
@@ -0,0 +1,12 @@
+import pandas
+
+# Keyword Method with iterrows()
+# {new_key:new_value for (index, row) in df.iterrows()}
+
+df = pandas.read_csv("nato_phonetic_alphabet.csv")
+phonetic_dict = {row.letter:row.code for (index, row) in df.iterrows()}
+
+#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+word = input("Enter a word: ").upper()
+code_list = [phonetic_dict[letter] for letter in word]
+print(code_list)
