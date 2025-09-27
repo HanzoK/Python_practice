642d7581bdce1d71260447238d252a7e8e4b69c0
HanzoK <hanju.soonsik.kim@gmail.com>
2025-09-27 07:01:09 +0200

    playing around with label, button, entry

--- intermediate/10_GUI_with_tkinter_and_function_args/main.py
+++ intermediate/10_GUI_with_tkinter_and_function_args/main.py
@@ -1,12 +1,30 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+window = Tk()
 window.title("My First GUI program")
 window.minsize(width=500, height=300)
 
-my_label = tkinter.Label(text="I am a label", font=("Arial", 24, "bold"))
-my_label.pack(side="left")
+# Label
 
+my_label = Label(text="I am a label", font=("Arial", 24, "bold"))
+my_label.pack()
+
+my_label["text"] = "New Text"
+my_label.config(text="New Text")
+
+# Button
+
+def button_clicked():
+    text_input = input.get()
+    my_label["text"] = text_input
+
+button = Button(text="Click Me", command=button_clicked)
+button.pack()
+
+# Entry
+
+input = Entry(width=10)
+input.pack()
 
 window.mainloop()
 
