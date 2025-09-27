4fae63a46cc5964c53bc73631289a0a9f34901c2
HanzoK <hanju.soonsik.kim@gmail.com>
2025-09-27 06:40:58 +0200

    started working on GUI with tkinter module. Also created playground file to test and practice with *args and **kwargs

---
+++ intermediate/10_GUI_with_tkinter_and_function_args/playground.py
@@ -0,0 +1,27 @@
+def add(*args):
+    total = 0
+    for n in args:
+        total += n
+    print(total)
+
+# add(1, 2, 3, 4, 5)
+
+def calculate(n, **kwargs):
+    print(kwargs)
+    n += kwargs["add"]
+    n *= kwargs["multiply"]
+    print(n)
+
+
+calculate(2, add=3, multiply=5)
+
+class Car:
+    def __init__(self, **kw):
+        self.make = kw.get("kw")
+        self.model = kw.get("model")
+        self.colour = kw.get("colour")
+        self.seats = kw.get("seats")
+
+my_car = Car(make="Nissan", model="GT-R")
+print(my_car.make)
+print(my_car.model)
