00566660325a405f177461672b740836a676996b
HanzoK <hanju.soonsik.kim@gmail.com>
2025-09-27 21:09:51 +0200

    played around with different tkinter widgets available. Also starting a distance converter mini-project

--- intermediate/10_GUI_with_tkinter_and_function_args/main.py
+++ intermediate/10_GUI_with_tkinter_and_function_args/main.py
@@ -1,30 +1,30 @@
 from tkinter import *
 
-window = Tk()
-window.title("My First GUI program")
-window.minsize(width=500, height=300)
-
-# Label
-
-my_label = Label(text="I am a label", font=("Arial", 24, "bold"))
-my_label.pack()
-
-my_label["text"] = "New Text"
-my_label.config(text="New Text")
-
-# Button
-
 def button_clicked():
     text_input = input.get()
     my_label["text"] = text_input
 
+window = Tk()
+window.title("My First GUI program")
+window.minsize(width=500, height=300)
+window.config(padx=20, pady=20)
+
+# Label
+my_label = Label(text="I am a label", font=("Arial", 24, "bold"))
+my_label["text"] = "New Text"
+my_label.grid(column=0, row=0)
+
+# Button 1
 button = Button(text="Click Me", command=button_clicked)
-button.pack()
+button.grid(column=1, row=1)
+
+# Button 2
+new_button = Button(text="New Button")
+new_button.grid(column=2, row=0)
 
 # Entry
-
 input = Entry(width=10)
-input.pack()
+input.grid(column=3, row=2)
 
 window.mainloop()
 
