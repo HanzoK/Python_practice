c973b474501b919f674cd09dd789a792d54dccd5
HanzoK <hanju.soonsik.kim@gmail.com>
2025-09-29 22:28:21 +0200

    adding a quick of raising my own exception

--- intermediate/13_errors_exceptions_and_JSON/practice/main.py
+++ intermediate/13_errors_exceptions_and_JSON/practice/main.py
@@ -1,21 +1,20 @@
 # FileNotFound
 
-try:
-    file = open("wow.txt")
-    a_dict = {"key":"value"}
-    print(a_dict["key"])
-except FileNotFoundError:
-    file = open("wow.txt", mode="w")
-    file.write("Why hello there!")
-except KeyError as error_msg:
-    print(f"Whoops, {error_msg} doesn't exist!")
-else:
-    content = file.read()
-    print(content)
-finally:
-    file.close()
-    print("File is finally closed")
-    
+# try:
+#     file = open("wow.txt")
+#     a_dict = {"key":"value"}
+#     print(a_dict["key"])
+# except FileNotFoundError:
+#     file = open("wow.txt", mode="w")
+#     file.write("Why hello there!")
+# except KeyError as error_msg:
+#     print(f"Whoops, {error_msg} doesn't exist!")
+# else:
+#     content = file.read()
+#     print(content)
+# finally:
+#     raise TypeError("This is an error I made up")
+
 # KeyError
 # a_dict = {"key": "value"}
 # value = a_dict{"what_key_is_this?"}
@@ -28,10 +27,11 @@ finally:
 # text = "wow"
 # print(text + 5)
 
-# try:
+height = float(input("Height: "))
+weight = int(input("Weight: "))
 
-# except:
+if height > 3:
+    raise ValueError("That's a ridiculous height for a human!")
 
-# else:
-
-# finally:
+bmi = weight/ height**2
+print(bmi)
