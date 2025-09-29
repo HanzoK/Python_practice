c4b1c0d8a2eeb2bc6d8cd3f3d92e28530742041f
HanzoK <hanju.soonsik.kim@gmail.com>
2025-09-29 22:43:29 +0200

    adding some more exception cases

--- intermediate/13_errors_exceptions_and_JSON/practice/main.py
+++ intermediate/13_errors_exceptions_and_JSON/practice/main.py
@@ -16,12 +16,37 @@
 #     raise TypeError("This is an error I made up")
 
 # KeyError
-# a_dict = {"key": "value"}
-# value = a_dict{"what_key_is_this?"}
+# facebook_posts = [
+#     {'Likes': 21, 'Comments': 2},
+#     {'Likes': 13, 'Comments': 2, 'Shares': 1},
+#     {'Likes': 33, 'Comments': 8, 'Shares': 3},
+#     {'Comments': 4, 'Shares': 2},
+#     {'Comments': 1, 'Shares': 1},
+#     {'Likes': 19, 'Comments': 3}
+# ]
+
+# def count_likes(posts):
+#     total_likes = 0
+#     for post in posts:
+#         try:
+#             total_likes = total_likes + post['Likes']
+#         except:
+#             print("There is an entry without any likes.")
+#     return total_likes
+
+# count_likes(facebook_posts)
 
 # IndexError
-# fruit_list = ["Apple", "Banana", "Pear"]
-# fruit = fruit_list[69]
+# fruits = ["Apple", "Pear", "Orange"]
+# def make_pie(index):
+#     try:
+#         fruit = fruits[index]
+#     except:
+#         fruit = "Fruit"
+#     finally:
+#         print(fruit + " pie")
+
+# make_pie(4)
 
 # TypeError
 # text = "wow"
