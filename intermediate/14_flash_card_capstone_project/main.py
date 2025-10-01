bd77dd7dce5d715203cee0762c7f7d59a27fb68e
HanzoK <hanju.soonsik.kim@gmail.com>
2025-10-01 15:08:40 +0200

    fixed a bug with timer that keeps going and flipping the cards no matter how many times the button has been pressed

--- intermediate/14_flash_card_capstone_project/main.py
+++ intermediate/14_flash_card_capstone_project/main.py
@@ -6,6 +6,7 @@ import math
 BACKGROUND_COLOR = "#B1DDC6"
 
 df = pandas.read_csv("./data/french_words.csv")
+not_solved_words = []
 
 def flip_card(translation):
     canvas.itemconfig(front_card_img, image=card_img_back)
@@ -13,17 +14,22 @@ def flip_card(translation):
     canvas.itemconfig(card_word, text=translation, fill="white")
 
 def generate_word():
+    global flip_timer
+    window.after_cancel(flip_timer)
     row = df.sample().iloc[0]
     new_french_word = row["French"]
     new_translation = row["English"]
+    not_solved_words.append(new_french_word)
     canvas.itemconfig(front_card_img, image=card_img_front)
     canvas.itemconfig(card_word, text=new_french_word, fill="black")
     canvas.itemconfig(card_title, text="French", fill="black")
-    window.after(3000, flip_card, new_translation)
+    flip_timer = window.after(3000, flip_card, new_translation)
 
 window = Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+
+flip_timer = window.after(3000, flip_card, new_translation)
 
 canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
 
@@ -42,8 +48,6 @@ correct_button.grid(column=1, row=1)
 incorrect_img = PhotoImage(file="./images/wrong.png")
 incorrect_button = Button(image=incorrect_img, highlightthickness=0, command=generate_word)
 incorrect_button.grid(column=0, row=1)
-
-
 
 
 window.mainloop()
