98d70c29f4cd849bab3d32c1ccfbfc82d2714ecf
HanzoK <hanju.soonsik.kim@gmail.com>
2025-10-01 03:17:11 +0200

    implemented window and graphics alongside generating french and english words on a flipcard graphic. Needs further functionality to be implemented

--- intermediate/14_flash_card_capstone_project/main.py
+++ intermediate/14_flash_card_capstone_project/main.py
@@ -1,2 +1,49 @@
+from tkinter import *
+import pandas
+import time
+import math
+
 BACKGROUND_COLOR = "#B1DDC6"
 
+df = pandas.read_csv("./data/french_words.csv")
+
+def flip_card(translation):
+    canvas.itemconfig(front_card_img, image=card_img_back)
+    canvas.itemconfig(card_title, text="English", fill="white")
+    canvas.itemconfig(card_word, text=translation, fill="white")
+
+def generate_word():
+    row = df.sample().iloc[0]
+    new_french_word = row["French"]
+    new_translation = row["English"]
+    canvas.itemconfig(front_card_img, image=card_img_front)
+    canvas.itemconfig(card_word, text=new_french_word, fill="black")
+    canvas.itemconfig(card_title, text="French", fill="black")
+    window.after(3000, flip_card, new_translation)
+
+window = Tk()
+window.title("Flashy")
+window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+
+canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
+
+card_img_front = PhotoImage(file="./images/card_front.png")
+card_img_back = PhotoImage(file="./images/card_back.png")
+front_card_img = canvas.create_image(400, 263, image=card_img_front)
+
+card_title = canvas.create_text(400, 150, text="Greetings!", font=("Ariel", 40, "italic"))
+card_word = canvas.create_text(400, 260, text="", font=("Ariel", 60, "bold"))
+canvas.grid(column=0, row=0, columnspan=2)
+
+correct_img = PhotoImage(file="./images/right.png")
+correct_button = Button(image=correct_img, highlightthickness=0, command=generate_word)
+correct_button.grid(column=1, row=1)
+
+incorrect_img = PhotoImage(file="./images/wrong.png")
+incorrect_button = Button(image=incorrect_img, highlightthickness=0, command=generate_word)
+incorrect_button.grid(column=0, row=1)
+
+
+
+
+window.mainloop()
