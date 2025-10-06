9c019c9f86c33aa322d84e50e80bccdb2c4f3827
HanzoK <hanju.soonsik.kim@gmail.com>
2025-10-07 00:37:02 +0200

    playing around with the datetime module. Decided to send myself motivational quotes via email using smtplib

--- intermediate_plus/00_smtplib_and_datetime/ABW/main.py
+++ intermediate_plus/00_smtplib_and_datetime/ABW/main.py
@@ -1,8 +1,29 @@
+# import datetime as dt
+
+# now = dt.datetime.now()
+# year = now.year
+# month = now.month
+# day_of_week = now.weekday()
+# print(year)
+
+# date_of_birth = dt.datetime(year=1995, month=12, day=15)
+# print(date_of_birth)
+
 import smtplib
 import os
 from dotenv import load_dotenv
+import datetime as dt
+import random
 
 load_dotenv()
+
+with open("quotes.txt") as quote_file:
+    quote_list = [quote for quote in quote_file]
+
+now = dt.datetime.now()
+current_weekday = now.weekday()
+
+quote_to_be_sent = random.choice(quote_list)
 
 my_email = os.getenv("SMTP_EMAIL")
 password = os.getenv("SMTP_PASSWORD")
@@ -15,7 +36,7 @@ with smtplib.SMTP("smtp.gmail.com") as connection:
     connection.sendmail(
         from_addr=my_email, 
         to_addrs=dest_email, 
-        msg="Subject:Hello\n\nThis is the body of my email."
+        msg=f"Subject:Motivational Quote\n\n{quote_to_be_sent}"
     )
 
 # connection.close()
