f2579972dbae1d891a5295b27cf2ff94824b388d
HanzoK <hanju.soonsik.kim@gmail.com>
2025-10-31 19:29:11 +0100

    Tweaked Weather alert to send me a message via SMS using Twilio API when it rains in my location. Checks the weather via OpenWeather API

--- intermediate_plus/03_API_keys__authentication_and_env/rain_alert/main.py
+++ intermediate_plus/03_API_keys__authentication_and_env/rain_alert/main.py
@@ -1,13 +1,18 @@
 import os
 from dotenv import load_dotenv
 import requests
+from twilio.rest import Client
 
 load_dotenv()
+account_sid = os.getenv("TWILIO_ACCOUNT_SID")
+auth_token = os.getenv("TWILIO_AUTH_TOKEN")
+my_number = os.getenv("MY_PHONE_NUMBER")
+twilio_number = os.getenv("TWILIO_NUMBER")
 
 parameters = {
     "lat": os.getenv("LATITUDE"),
     "lon": os.getenv("LONGITUDE"),
-    "cnt": 4,
+    "cnt": 12,
     "appid": os.getenv("API_KEY"),
 }
 
@@ -15,4 +20,16 @@ response = requests.get(url="https://api.openweathermap.org/data/2.5/forecast", 
 response.raise_for_status()
 data = response.json()
 
-print(data)
+will_it_rain = False
+for hour in data["list"]:
+    condition_code = hour["weather"][0]["id"]
+    if int(condition_code) < 700:
+        will_it_rain = True
+if will_it_rain:
+    client = Client(account_sid, auth_token)
+    message = client.messages.create(
+    body="It's going to rain. Please take care of yourself and bring an umbrella with you. ;)",
+    from_=twilio_number,
+    to=my_number,
+    )
+    print(message.status)
