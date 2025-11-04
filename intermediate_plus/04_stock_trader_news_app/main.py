1aeae9523c7e2ed58837b0057e2bd76a77128103
HanzoK <hanju.soonsik.kim@gmail.com>
2025-11-04 21:15:41 +0100

    Finished Stock trading news app. Checks for stock changes of over 5% (positive/negative), scrapes the latest 3 news pieces of affected company and sends them to my phone number via Twilio API Endpoint

--- intermediate_plus/04_stock_trader_news_app/main.py
+++ intermediate_plus/04_stock_trader_news_app/main.py
@@ -1,28 +1,40 @@
 import os
 import requests
 from dotenv import load_dotenv
+from twilio.rest import Client
 
 load_dotenv()
 
 STOCK = "TSLA"
-COMPANY_NAME = "Tesla Inc"
-ALPHA_KEY = os.getenv("STOCK_API_KEY")
 THRESHOLD_CHANGE = 5.0 # Minimum % change required to trigger news message
+ALPHA_KEY = os.getenv("STOCK_API_KEY")
+NEWS_KEY = os.getenv("NEWS_API_KEY")
+ACCOUNT_SID = os.getenv("TWILIO_ACCOUNT_SID")
+AUTH_TOKEN = os.getenv("TWILIO_AUTH_TOKEN")
+MY_NUMBER = os.getenv("MY_PHONE_NUMBER")
+TWILIO_NUMBER = os.getenv("TWILIO_NUMBER")
 
-url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={STOCK}&apikey={ALPHA_KEY}"
+stock_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={STOCK}&apikey={ALPHA_KEY}"
+news_url = f"https://newsapi.org/v2/everything?q=TSLA&apiKey={NEWS_KEY}"
 
 required_vars = {
     "STOCK_API_KEY": ALPHA_KEY,
+    "NEWS_API_KEY": NEWS_KEY,
+    "TWILIO_ACCOUNT_SID": ACCOUNT_SID,
+    "TWILIO_AUTH_TOKEN": AUTH_TOKEN,
+    "MY_PHONE_NUMBER": MY_NUMBER,
+    "TWILIO_NUMBER": TWILIO_NUMBER,
 }
 
 missing = [item for (item, value) in required_vars.items() if not value]
 if missing:
-    raise ValueError(f"Missing environment variables: {', '.join(missing)}. Please check your .env file.")
+    raise ValueError(f"Missing environment variables: {', '.join(missing)}. Check your .env file for the mising information!")
 
-response = requests.get(url)
-data = response.json()
+stock_response = requests.get(stock_url)
+stock_response.raise_for_status()
+stock_data = stock_response.json()
 
-time_series = data["Time Series (Daily)"]
+time_series = stock_data["Time Series (Daily)"]
 historical_dates = sorted(time_series.keys(), reverse=True)
 latest_date = historical_dates[0]
 previous_date = historical_dates[1]
@@ -31,29 +43,28 @@ latest_close = float(time_series[latest_date]["4. close"])
 previous_close = float(time_series[previous_date]["4. close"])
 
 price_change = round((latest_close - previous_close) / previous_close * 100, 2)
+moved_enough = abs(price_change) >= THRESHOLD_CHANGE
 
-moved_enough = price_change >= THRESHOLD_CHANGE
+direction = "🔺+" if price_change > 0 else "🔻-"
 
 if moved_enough:
-    print("Get News")
+    news_response = requests.get(news_url)
+    news_response.raise_for_status()
+    news_data = news_response.json()
+    articles = news_data["articles"]
+    latest_news_pieces = articles[:3]
+    latest_three = [(article["title"], article["description"]) for article in latest_news_pieces]
+
+    premade_message = f"{STOCK}: {direction}{price_change}"
+    for item in latest_three:
+        premade_message += f"\n\nHeadline: {item[0]}\nBrief:{item[1]}"
+
+    client = Client(ACCOUNT_SID, AUTH_TOKEN)
+    message = client.messages.create(
+    body=premade_message,
+    from_=TWILIO_NUMBER,
+    to=MY_NUMBER,
+    )
+    print(message.status)
 else:
-    pass
-
-## STEP 2: Use https://newsapi.org
-# Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
-
-## STEP 3: Use https://www.twilio.com
-# Send a seperate message with the percentage change and each article's title and description to your phone number. 
-
-
-#Optional: Format the SMS message like this: 
-"""
-TSLA: 🔺2%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-or
-"TSLA: 🔻5%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-"""
-
+    print(f"No significant changes over selected threshold of {THRESHOLD_CHANGE}%.")
